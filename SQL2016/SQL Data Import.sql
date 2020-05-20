7c6d473c8e313574c67bac6cdf8145c137ae4d6a
Agile Flex Agency <agile@agile-flex.com>
2020-05-20 12:01:23 +0300

    IMM Invest RPA 20-05-2020

---
+++ SQL2016/SQL Data Import.sql
@@ -0,0 +1,37 @@
+﻿--sp_configure 'show advanced options', 1;  
+--RECONFIGURE;
+--GO 
+--sp_configure 'Ad Hoc Distributed Queries', 1;  
+--RECONFIGURE;  
+--GO  
+
+--EXEC master.[sys].[sp_MSset_oledb_prop] N'Microsoft.ACE.OLEDB.16.0', N'AllowInProcess', 1
+
+USE IMMInvest
+
+DECLARE @SQLText NVARCHAR(MAX) 
+DECLARE @FileName VARCHAR(MAX) 
+
+SET NOCOUNT ON;
+
+TRUNCATE TABLE Requests;
+
+INSERT INTO Requests 
+(
+	[Rue],
+	[Data],
+	[CuiCif],
+	[Firma],
+	[StareSolicitare]
+)
+--OPENROWSET
+SELECT
+	[Rue]					AS [Rue],
+	[Dată înregistrare]		AS [Data],
+	[CUI/CIF]				AS [CuiCif],
+	[Firmă]					AS [Firma],
+	[Stare solicitare]		AS [StareSolicitare]
+FROM 
+	OPENROWSET('Microsoft.ACE.OLEDB.16.0', 'Excel 12.0 Xml;Database=D:\UIPathRPA\ImmInvestRequests\Temp\imm-invest-requests-20-5-2020.xlsx', Sheet1$);
+
+EXEC sp_getUniqueRequests
